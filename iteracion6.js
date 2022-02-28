4d82432476c9aa7e171c9eb96e12c8fdc9a05a79
Raul <61325276+KeywizZ@users.noreply.github.com>
2022-02-28 16:31:48 +0100

    Iteracion #6 y final

---
+++ iteracion6.js
@@ -0,0 +1,30 @@
+// Bucles
+
+//1.1
+console.log('Ejercicio 1.1')
+// ↑ Para separar resultados ↑
+
+let i;
+
+for (i=0; i<10; i++) {
+    console.log(i);
+}
+
+// 1.2
+console.log('Ejercicio 1.2')
+// ↑ Para separar resultados ↑
+
+for (i=0; i<10; i++) {
+    if (i%2 == 0) {
+        console.log(i);
+    }
+}
+
+// 1.3
+console.log('Ejercicio 1.3')
+// ↑ Para separar resultados ↑
+
+for (i=0; i<10; i++) {
+    console.log("Intentando dormir...");
+}
+console.log('Dormido!');
